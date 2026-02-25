c7f9b33f45b3700ca7d39fea901e8b1d20f02a69
emparcm <e.carrasquermoya@edu.gva.es>
2026-02-25 08:19:33 +0100

    First Commit

---
+++ src/main/java/es/progcipfpbatoi/prog/sa7/pistes/PistesApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.progcipfpbatoi.prog.sa7.pistes;
+
+/**
+ *
+ * @author batoi
+ */
+public class PistesApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
