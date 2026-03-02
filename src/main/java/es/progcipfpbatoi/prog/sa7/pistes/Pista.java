0baf5c81b2cc8fd133efca96b64488dc4de663a7
javaleper2 <javaleper2@alu.edu.gva.es>
2026-03-02 12:03:33 +0100

    clase pista archivo guardado

--- src/main/java/es/progcipfpbatoi/prog/sa7/pistes/Pista.java
+++ src/main/java/es/progcipfpbatoi/prog/sa7/pistes/Pista.java
@@ -1,6 +1,46 @@
 
 package es.progcipfpbatoi.prog.sa7.pistes;
 
-public class Pista {
+import java.util.Objects;
 
+public class Pista {
+    private int id;
+    private String descripcion;
+    private Categorias categoria;
+    private int confiabilidad;
+    private String fecha;
+
+    //constructor
+    public Pista(int id, String descripcion, Categorias categoria, int confiabilidad, String fecha) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+        this.confiabilidad = confiabilidad;
+        this.fecha = fecha;
+    }
+
+    @Override
+    public String toString() {
+        return "Pista{" + "id=" + id + ", descripcion=" + descripcion + ", categoria=" + categoria + ", confiabilidad=" + confiabilidad + ", fecha=" + fecha + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pista other = (Pista) obj;
+        if (!Objects.equals(this.descripcion, other.descripcion)) {
+            return false; //falta comparar solo el 80% del contenido
+        }
+        return Objects.equals(this.categoria, other.categoria);
+    }
+    
+    
 }
