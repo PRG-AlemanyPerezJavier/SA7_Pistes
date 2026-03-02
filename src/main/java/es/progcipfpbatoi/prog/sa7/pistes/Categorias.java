d656098e91e2654093290e1442fdffdf752cef1d
javaleper2 <javaleper2@alu.edu.gva.es>
2026-03-02 11:53:00 +0100

    test con enum

---
+++ src/main/java/es/progcipfpbatoi/prog/sa7/pistes/Categorias.java
@@ -0,0 +1,9 @@
+package es.progcipfpbatoi.prog.sa7.pistes;
+
+/**
+ *
+ * @author jalpe
+ */
+public enum Categorias {
+    GOBIERNO, TECNOLOGIA, MISTERIO, CORRUPCION
+}
--- src/main/java/es/progcipfpbatoi/prog/sa7/pistes/PistesApp.java
+++ src/main/java/es/progcipfpbatoi/prog/sa7/pistes/PistesApp.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package es.progcipfpbatoi.prog.sa7.pistes;
 
-/**
- *
- * @author batoi
- */
 public class PistesApp {
 
     public static void main(String[] args) {
